0c8a1eeb33ebe6285af259788593608b04afef76
nkgrr1004 <nikhilrrgupta.com>
2022-03-19 14:28:30 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import { useState  } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { AddTodoActions, RemoveTodoAction } from './actions/TodoActions';
 
 function App() {
+  const [todo,setTodo] = useState();
+  const dispatch = useDispatch();
+
+  const Todo = useSelector((state) => state.Todo);
+
+  const {todos} = Todo;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(AddTodoActions(todo))
+  };
+
+ const removeHandler = (t) => {
+dispatch(RemoveTodoAction(t))
+ }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Todo App</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+        placeholder="Add Todo" 
+        style={{
+          width: 350,
+          padding: 10,
+          borderRadius: 20,
+          border: 'none',
+          fontSize: 20,
+        }}
+        onChange={(e) => setTodo(e.target.value)}
+        />
+     
+      <button 
+      type="submit"
+      style={{
+        padding: 12,
+        borderRadius: 25,
+        fontSize: 15,
+        marginLeft: 20,
+      }}
+      >Go</button>
+       </form>
+       <ul className="allTodos"> 
+       {
+         todos && todos.map((t) => (
+          <li key={t.id} className="singleTodo">
+          <span className="todoText ">{t.todo}</span>
+          <button
+          style={{
+            borderRadius: 25,
+            padding: 10,
+            border: "1px solid red",
+            color: "white",
+            background: "orangered",
+          }}
+          onClick={() => removeHandler(t)}
+       >
+             Delete
+           </button>
+         </li>
+         ))}
+       </ul>
+    </header>
+    </div> 
   );
 }
 
